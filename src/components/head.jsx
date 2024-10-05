1889530dca7b8d57e9695ee06b039ffa9e85de1c
Abeloth19 <prajwalmahajan258@gmail.com>
2024-10-05 21:14:16 +0530

    header responsiveness fix

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -14,7 +14,11 @@ const Head = () => {
       />
       <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'></div>
       <div className='absolute top-0  w-full h-full flex flex-col justify-center   text-white p-4'>
-        <h1 className=' text-[80px] mb-4 text-[#B1D8B7] '>Escape, Explore, Experience</h1>
+        <div className=' text-[80px]  text-[#B1D8B7] flex lg:flex-row flex-col lg:space-x-2 space-y-2'>
+          <p>Escape,</p> 
+          <p> Explore,</p> 
+          <p> Experience</p> 
+          </div>        
         <h2 className='py-4 text-[#FFD8BF] text-md'>Travel Beyond Boundaries</h2>
      
       </div>
